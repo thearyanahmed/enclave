550b7843b6308b6ec6d772813040f80b6ee5f6e4
Aryan <paryan@digitalocean.com>
2025-07-03 18:07:30 +0100

    Add test for login action

--- src/actions/login.rs
+++ src/actions/login.rs
@@ -30,3 +30,33 @@ fn verify_password(password: &str, hashed: &str) -> Result<bool, AuthError> {
         .map_err(|e| AuthError::Other(e.to_string()))
 }
 
+#[cfg(test)]
+mod tests {
+    use crate::{ MockUserRepository, User};
+    use super::*;
+    use rand::rngs::OsRng;
+    use argon2::{Argon2, PasswordHasher};
+    use password_hash::SaltString;
+
+    #[tokio::test]
+    async fn test_login_action() {
+        let repo = MockUserRepository::new();
+
+        let salt = SaltString::generate(&mut OsRng);
+        let argon2 = Argon2::default();
+
+        let password = "securepassword";
+
+        let hashed = argon2
+            .hash_password(password.as_bytes(), &salt)
+            .map_err(|e| AuthError::Other(e.to_string()))
+            .map(|hash| hash.to_string());
+
+        let user = User::mock_from_credentials("user@email.com", hashed.unwrap().as_str());
+        repo.users.lock().unwrap().push(user);
+
+        let login = LoginAction::new(repo);
+        let logged_in_user = login.execute("user@email.com", "securepassword").await;
+        assert!(logged_in_user.is_ok());
+    }
+}
