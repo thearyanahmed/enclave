a8e124ee7b129cdb639158d90a8bb2dde943c4e3
Aryan <paryan@digitalocean.com>
2025-12-22 16:14:10 +0000

    Add jwt hanlder middleware

--- src/api/actix/mod.rs
+++ src/api/actix/mod.rs
@@ -1,7 +1,11 @@
 pub mod cors;
 mod handlers;
+#[cfg(feature = "jwt")]
+mod jwt_middleware;
 mod middleware;
 mod routes;
 
+#[cfg(feature = "jwt")]
+pub use jwt_middleware::{JwtAuthenticatedUser, JwtAuthenticationError};
 pub use middleware::{AuthenticatedUser, AuthenticationError, extract_bearer_token};
 pub use routes::{auth_routes, private_routes, public_routes};
