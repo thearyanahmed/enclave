5dd3b4e8cfaaeabf885d7ff25a5276d7e1c9b134
Aryan <paryan@digitalocean.com>
2025-12-22 16:12:47 +0000

    Add jwt login action

--- src/actions/mod.rs
+++ src/actions/mod.rs
@@ -2,6 +2,8 @@ pub mod change_password;
 pub mod delete_user;
 pub mod forgot_password;
 pub mod get_user;
+#[cfg(feature = "jwt")]
+pub mod jwt_login;
 pub mod login;
 pub mod logout;
 pub mod refresh_token;
@@ -15,6 +17,8 @@ pub use change_password::ChangePasswordAction;
 pub use delete_user::DeleteUserAction;
 pub use forgot_password::ForgotPasswordAction;
 pub use get_user::GetUserAction;
+#[cfg(feature = "jwt")]
+pub use jwt_login::{JwtLoginAction, JwtLoginResponse};
 pub use login::{LoginAction, LoginConfig};
 pub use logout::LogoutAction;
 pub use refresh_token::RefreshTokenAction;
